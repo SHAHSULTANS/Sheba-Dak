74673c439e41767935651c96df5ebeb11c12d014
shanto <sultan91234567@gmail.com>
2025-09-22 15:39:43 +0600

    Initialize Flutter project with .gitignore and default setup

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'core/theme/app_theme.dart';
+import 'features/auth/presentation/bloc/auth_bloc.dart';
+import 'routes.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(create: (context) => AuthBloc()),
+      ],
+      child: MyApp(prefs: prefs),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  final SharedPreferences prefs;
+
+  const MyApp({super.key, required this.prefs});
+
+  @override
+  Widget build(BuildContext context) {
+    return Builder(
+      builder: (context) {
+        return MaterialApp.router(
+          title: 'SmartSheba',
+          theme: AppTheme.lightTheme,
+          routerConfig: appRouter,
+          debugShowCheckedModeBanner: false,
+        );
+      },
+    );
+  }
+}
